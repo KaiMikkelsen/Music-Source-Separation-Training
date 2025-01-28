ef4d1c0cbd8e1a6a50d386518f9624b058426d25
KaiMikkelsen <squabee@gmail.com>
2025-01-28 16:14:19 -0500

    Refactor run_mdx23c_training.sh to use SLURM_TMPDIR for scratch directory, enhancing temporary storage management.

--- run_mdx23c_training.sh
+++ run_mdx23c_training.sh
@@ -10,7 +10,8 @@ module load python/3.10 cuda/12.2 cudnn/8.9.5.29
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
 
 CURRENT_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-SCRATCH_DIR="/scratch/$USER/$SLURM_JOB_ID"
+SCRATCH_DIR=$SLURM_TMPDIR
+
 
 # Variables
 MODEL_TYPE="mdx23c"
