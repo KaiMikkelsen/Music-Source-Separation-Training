66097f4b26df2384a43ec7945da1afc984ba40de
KaiMikkelsen <squabee@gmail.com>
2025-01-31 16:24:28 -0500

    Add zip_musdb.sh script for zipping MUSDB18HQ dataset with SLURM job configuration

---
+++ zip_musdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6        # Adjust based on your cluster's CPU/GPU ratio
+#SBATCH --mem=125G               # Adjust memory as needed
+#SBATCH --time=0-03:00:00           # DD-HH:MM:SS
+#SBATCH --account=def-ichiro
+#SBATCH --output=slurm_logs/slurm-%j.out  # Use Job ID for unique output files
+
+
+
+zip -r MUSDB18HQ.zip MUSDB18HQ
+
+
