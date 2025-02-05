8fafced826cffdca354ae8196ec93d032424db47
KaiMikkelsen <squabee@gmail.com>
2025-02-05 10:45:40 -0500

    wandb ahent

---
+++ run_wandb_agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6        # Adjust based on your cluster's CPU/GPU ratio
+#SBATCH --mem=125G               # Adjust memory as needed
+#SBATCH --time=4-00:00           # DD-HH:MM:SS
+#SBATCH --account=def-ichiro
+#SBATCH --output=slurm_logs/slurm-%j.out  # Use Job ID for unique output files
+
+wandb agent kaimikkelsen-mcgill-university/msst/fam6ndv9
