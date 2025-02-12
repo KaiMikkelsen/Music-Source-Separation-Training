f594ffe2c4fb3859cc31769b28eaa57e063759c4
KaiMikkelsen <squabee@gmail.com>
2025-02-11 17:39:18 -0800

    Update SLURM job output file naming for stemmoisesdb processing

--- job_create4stemmoises.sh
+++ job_create4stemmoises.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=125G               # Adjust memory as needed
 #SBATCH --time=3-00:00           # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
-#SBATCH --output=slurm_logs/slurm-%j.out  # Use Job ID for unique output files
+#SBATCH --output=create_4_stemmoisesdb.out  # Use Job ID for unique output files
 
 module load python/3.10 cuda/12.2 cudnn/8.9.5.29
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
