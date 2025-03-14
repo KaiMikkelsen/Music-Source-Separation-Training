ff3420906ca58d1a2b74cb12687eb0e52e013445
KaiMikkelsen <squabee@gmail.com>
2025-03-14 15:25:47 -0400

    Update configuration path in run_scnet_training.sh to use the correct config file

--- run_scnet_training.sh
+++ run_scnet_training.sh
@@ -14,7 +14,7 @@ SCRATCH_DIR=$SLURM_TMPDIR
 
 # Variables
 MODEL_TYPE="scnet"
-CONFIG_PATH="configs/optimized_configs/config_musdb18_scnet.yaml"
+CONFIG_PATH="configs/config_musdb18_scnet.yaml"
 DATASET_NAME="MUSDB18HQ"
 DATASET_ZIP="../data/$DATASET_NAME.zip" # Specify the dataset ZIP name
 SLURM_LOGS_PATH="slurm_logs/${MODEL_TYPE}_${CURRENT_DATE}"
