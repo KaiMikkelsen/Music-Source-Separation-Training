5617e887e66150b17be143d8f4d491bdcaaf5ac0
KaiMikkelsen <squabee@gmail.com>
2025-05-09 08:47:21 -0400

    Update configuration path and set dataset path for training script

--- run_bs_roformer_training.sh
+++ run_bs_roformer_training.sh
@@ -14,7 +14,7 @@ SCRATCH_DIR=$SLURM_TMPDIR
 
 # Variables
 MODEL_TYPE="bs_roformer"
-CONFIG_PATH="configs/config_musdb18_bs_roformer.yaml"
+CONFIG_PATH="configs/config_musdb18_bs_roformer_384_8_2_485100_sage.yaml"
 DATASET_NAME="MUSDB18HQ"
 DATASET_ZIP="/home/kaim/scratch/$DATASET_NAME.zip" # Specify the dataset ZIP name
 SLURM_LOGS_PATH="slurm_logs/${MODEL_TYPE}_${CURRENT_DATE}"
@@ -32,51 +32,53 @@ exec > >(tee -a "$SLURM_LOGS_PATH/slurm-${SLURM_JOB_ID}.out") 2>&1
 # Activate the environment
 source separation_env/bin/activate
 
-RUNNING_ON_MAC=False
-if [ "$RUNNING_ON_MAC" = False ]; then
+# RUNNING_ON_MAC=False
+# if [ "$RUNNING_ON_MAC" = False ]; then
 
-    # Move and unzip dataset to scratch directory
-    if [ ! -f "$SCRATCH_DIR/$(basename "$DATASET_ZIP")" ]; then
-        echo "Moving $DATASET_ZIP to $SCRATCH_DIR for faster access"
-        cp "$DATASET_ZIP" "$SCRATCH_DIR"
-    else
-        echo "Dataset already exists in $SCRATCH_DIR, skipping copy."
-    fi
+#     # Move and unzip dataset to scratch directory
+#     if [ ! -f "$SCRATCH_DIR/$(basename "$DATASET_ZIP")" ]; then
+#         echo "Moving $DATASET_ZIP to $SCRATCH_DIR for faster access"
+#         cp "$DATASET_ZIP" "$SCRATCH_DIR"
+#     else
+#         echo "Dataset already exists in $SCRATCH_DIR, skipping copy."
+#     fi
 
-    DATASET_ZIP_BASENAME=$(basename "$DATASET_ZIP")
-    SCRATCH_ZIP="$SCRATCH_DIR/$DATASET_ZIP_BASENAME"
+#     DATASET_ZIP_BASENAME=$(basename "$DATASET_ZIP")
+#     SCRATCH_ZIP="$SCRATCH_DIR/$DATASET_ZIP_BASENAME"
 
-    # mkdir -p "$SCRATCH_DIR/$DATASET_NAME"
-    # echo "created directory $SCRATCH_DIR/$DATASET_NAME"
-    # echo "Unzipping dataset in $SCRATCH_DIR/$DATASET_NAME"
+#     # mkdir -p "$SCRATCH_DIR/$DATASET_NAME"
+#     # echo "created directory $SCRATCH_DIR/$DATASET_NAME"
+#     # echo "Unzipping dataset in $SCRATCH_DIR/$DATASET_NAME"
 
-    echo "unzipping $SCRATCH_DIR/$DATASET_NAME.zip"
-    unzip "$SCRATCH_DIR/$DATASET_NAME.zip" -d "$SCRATCH_DIR"
+#     echo "unzipping $SCRATCH_DIR/$DATASET_NAME.zip"
+#     unzip "$SCRATCH_DIR/$DATASET_NAME.zip" -d "$SCRATCH_DIR"
 
 
-    # if ! unzip -q "$SCRATCH_ZIP" -d "$SCRATCH_DIR/$DATASET_NAME"; then
-    #     echo "zip failed"
-    # fi
+#     # if ! unzip -q "$SCRATCH_ZIP" -d "$SCRATCH_DIR/$DATASET_NAME"; then
+#     #     echo "zip failed"
+#     # fi
 
-    echo "Dataset successfully unzipped."
+#     echo "Dataset successfully unzipped."
 
-    if [ "$DATASET_NAME" = "MOISESDB" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/moisesdb/moisesdb_v0.1"
-    elif [ "$DATASET_NAME" = "MUSDB18HQ" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME"
-    elif [ "$DATASET_NAME" = "SDXDB23_Bleeding" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb12_bleeding"
-    elif [ "$DATASET_NAME" = "SDXDB23_LabelNoise" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb23_labelnoise"
-    else
-        echo "Unknown dataset: $DATASET_NAME"
-        exit 1
-    fi
+#     if [ "$DATASET_NAME" = "MOISESDB" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/moisesdb/moisesdb_v0.1"
+#     elif [ "$DATASET_NAME" = "MUSDB18HQ" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME"
+#     elif [ "$DATASET_NAME" = "SDXDB23_Bleeding" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb12_bleeding"
+#     elif [ "$DATASET_NAME" = "SDXDB23_LabelNoise" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb23_labelnoise"
+#     else
+#         echo "Unknown dataset: $DATASET_NAME"
+#         exit 1
+#     fi
 
-else
-    DATA_PATH="../data/$DATASET_NAME"
-    echo "Running on Mac. Skipping dataset unzipping."
-fi
+# else
+#     DATA_PATH="../data/$DATASET_NAME"
+#     echo "Running on Mac. Skipping dataset unzipping."
+# fi
+
+DATA_PATH="/Users/kaimikkelsen/canada_compute/data/$DATASET_NAME"
 
 echo "Dataset path set to: $DATA_PATH"
 
