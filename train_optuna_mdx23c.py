c45f11859ce432de506aedf466ad8e75335d51de
KaiMikkelsen <squabee@gmail.com>
2025-05-13 08:17:37 -0400

    Refactor import statements to include additional model utility functions for enhanced functionality

--- train_optuna_mdx23c.py
+++ train_optuna_mdx23c.py
@@ -26,7 +26,8 @@ from utils.dataset import MSSDataset
 from utils.settings import get_model_from_config, parse_args_inference
 from valid import valid_multi_gpu, valid
 
-from utils import bind_lora_to_model, load_start_checkpoint
+from utils.model_utils import bind_lora_to_model, load_start_checkpoint, save_weights, normalize_batch, \
+    initialize_model_and_device, get_optimizer, save_last_weights
 import loralib as lora
 import optuna
 from optuna.trial import Trial
