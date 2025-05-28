c8fc016111c198788f0025acd196ea7e8552528f
KaiMikkelsen <squabee@gmail.com>
2025-05-28 10:50:13 -0400

    Update SCNet configuration and training scripts: refine audio parameters, adjust dataset paths, and optimize training settings

--- optimized_training_scripts/split3/run_scnet_training.sh
+++ optimized_training_scripts/split3/run_scnet_training.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6        # Adjust based on your cluster's CPU/GPU ratio
 #SBATCH --mem=125G               # Adjust memory as needed
-#SBATCH --time=7-00:00           # DD-HH:MM:SS
+#SBATCH --time=5-00:00           # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 #SBATCH --output=slurm_logs/slurm-%j.out  # Use Job ID for unique output files
 
@@ -14,14 +14,16 @@ SCRATCH_DIR=$SLURM_TMPDIR
 
 git pull
 
+
 # Variables
 MODEL_TYPE="scnet"
 CONFIG_PATH="configs/optimized_configs/config_musdb18_scnet.yaml"
-DATA_HOME_PATH="/home/kaim/projects/def-ichiro/kaim/data"
+#DATA_HOME_PATH="/home/kaim/projects/def-ichiro/kaim/data"
 DATASET_NAME="MUSDB18HQ"
-DATASET_ZIP="$DATA_HOME_PATH/$DATASET_NAME.zip" # Specify the dataset ZIP name
+#DATASET_ZIP="$DATA_HOME_PATH/$DATASET_NAME.zip" # Specify the dataset ZIP name
 SLURM_LOGS_PATH="slurm_logs/${MODEL_TYPE}_${CURRENT_DATE}"
 CHECKPOINTS_PATH="/home/kaim/scratch/checkpoints/${MODEL_TYPE}_${CURRENT_DATE}"
+DATA_PATH="/home/kaim/scratch/MUSDB18_ALL/MUSDB18_HQ_3"
 
 # Create necessary directories
 #mkdir -p "$SCRATCH_DIR"
@@ -33,73 +35,73 @@ exec > >(tee -a "$SLURM_LOGS_PATH/slurm-${SLURM_JOB_ID}.out") 2>&1
 # Activate the environment
 source separation_env/bin/activate
 
-RUNNING_ON_MAC=false
+RUNNING_ON_MAC=False
 
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    RUNNING_ON_MAC=true
-else
-    RUNNING_ON_MAC=false
-fi
+# if [[ "$(uname)" == "Darwin" ]]; then
+#     RUNNING_ON_MAC=true
+# else
+#     RUNNING_ON_MAC=false
+# fi
 
-echo "Running on Mac: $RUNNING_ON_MAC"
-
-
-if [[ "$RUNNING_ON_MAC" == "false" ]]; then
-
-    mkdir -p "$SLURM_LOGS_PATH"
-
-    mkdir -p "$CHECKPOINTS_PATH"
-
-    # Move and unzip dataset to scratch directory
-    if [ ! -f "$SCRATCH_DIR/$(basename "$DATASET_ZIP")" ]; then
-        echo "Moving $DATASET_ZIP to $SCRATCH_DIR for faster access"
-        cp "$DATASET_ZIP" "$SCRATCH_DIR"
-    else
-        echo "Dataset already exists in $SCRATCH_DIR, skipping copy."
-    fi
-
-    DATASET_ZIP_BASENAME=$(basename "$DATASET_ZIP")
-    SCRATCH_ZIP="$SCRATCH_DIR/$DATASET_ZIP_BASENAME"
-
-    # mkdir -p "$SCRATCH_DIR/$DATASET_NAME"
-    # echo "created directory $SCRATCH_DIR/$DATASET_NAME"
-    # echo "Unzipping dataset in $SCRATCH_DIR/$DATASET_NAME"
-
-    echo "unzipping $SCRATCH_DIR/$DATASET_NAME.zip"
-    unzip "$SCRATCH_DIR/$DATASET_NAME.zip" -d "$SCRATCH_DIR"
+# echo "Running on Mac: $RUNNING_ON_MAC"
 
 
-    # if ! unzip -q "$SCRATCH_ZIP" -d "$SCRATCH_DIR/$DATASET_NAME"; then
-    #     echo "zip failed"
-    # fi
 
-    echo "Dataset successfully unzipped."
+# if [[ "$RUNNING_ON_MAC" == "false" ]]; then
 
-    if [ "$DATASET_NAME" = "MOISESDB" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/moisesdb/moisesdb_v0.1"
-    elif [ "$DATASET_NAME" = "MUSDB18HQ" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME"
-    elif [ "$DATASET_NAME" = "SDXDB23_Bleeding" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb12_bleeding"
-    elif [ "$DATASET_NAME" = "SDXDB23_LabelNoise" ]; then
-        DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb23_labelnoise"
-    else
-        echo "Unknown dataset: $DATASET_NAME"
-        exit 1
-    fi
+#     mkdir -p "$SLURM_LOGS_PATH"
 
-else
-    DATA_PATH="/Users/kaimikkelsen/canada_compute/data/$DATASET_NAME"
-    echo "Running on Mac. Skipping dataset unzipping."
-fi
+#     mkdir -p "$CHECKPOINTS_PATH"
+
+#     # Move and unzip dataset to scratch directory
+#     if [ ! -f "$SCRATCH_DIR/$(basename "$DATASET_ZIP")" ]; then
+#         echo "Moving $DATASET_ZIP to $SCRATCH_DIR for faster access"
+#         cp "$DATASET_ZIP" "$SCRATCH_DIR"
+#     else
+#         echo "Dataset already exists in $SCRATCH_DIR, skipping copy."
+#     fi
+
+#     DATASET_ZIP_BASENAME=$(basename "$DATASET_ZIP")
+#     SCRATCH_ZIP="$SCRATCH_DIR/$DATASET_ZIP_BASENAME"
+
+#     # mkdir -p "$SCRATCH_DIR/$DATASET_NAME"
+#     # echo "created directory $SCRATCH_DIR/$DATASET_NAME"
+#     # echo "Unzipping dataset in $SCRATCH_DIR/$DATASET_NAME"
+
+#     echo "unzipping $SCRATCH_DIR/$DATASET_NAME.zip"
+#     unzip "$SCRATCH_DIR/$DATASET_NAME.zip" -d "$SCRATCH_DIR"
+
+
+#     # if ! unzip -q "$SCRATCH_ZIP" -d "$SCRATCH_DIR/$DATASET_NAME"; then
+#     #     echo "zip failed"
+#     # fi
+
+#     echo "Dataset successfully unzipped."
+
+#     if [ "$DATASET_NAME" = "MOISESDB" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/moisesdb/moisesdb_v0.1"
+#     elif [ "$DATASET_NAME" = "MUSDB18HQ" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME"
+#     elif [ "$DATASET_NAME" = "SDXDB23_Bleeding" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb12_bleeding"
+#     elif [ "$DATASET_NAME" = "SDXDB23_LabelNoise" ]; then
+#         DATA_PATH="$SCRATCH_DIR/$DATASET_NAME/sdxdb23_labelnoise"
+#     else
+#         echo "Unknown dataset: $DATASET_NAME"
+#         exit 1
+#     fi
+
+# else
+#     DATA_PATH="/Users/kaimikkelsen/canada_compute/data/$DATASET_NAME"
+#     echo "Running on Mac. Skipping dataset unzipping."
+# fi
+
 
 echo "Dataset path set to: $DATA_PATH"
 
 
 echo "Running training script for model: $MODEL_TYPE with dataset at $DATA_PATH"
-
-# do k folds??
 
 python train.py \
     --model_type "$MODEL_TYPE" \
@@ -107,14 +109,9 @@ python train.py \
     --results_path "$CHECKPOINTS_PATH" \
     --data_path "$DATA_PATH/train" \
     --valid_path "$DATA_PATH/validation" \
-    --metrics sdr si_sdr l1_freq log_wmse aura_stft aura_mrstft bleedless fullness \
+    --metrics sdr si_sdr aura_mrstft\
     --num_workers 4 \
     --start_check_point "" \
     --device_ids 0 \
     --wandb_key 689bb384f0f7e0a9dbe275c4ba6458d13265990d
-
-
-# python run test set VALID.PY
-
-
 
